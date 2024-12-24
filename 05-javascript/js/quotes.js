42271ab0b3852b7acb4e99e13f33d2f733728169
Ali Afghah <ali.afghah@gmail.com>
2024-12-24 13:36:11 +0330

    feat: quotes project is done

--- 05-javascript/js/quotes.js
+++ 05-javascript/js/quotes.js
@@ -1,25 +1,64 @@
 import KEY from './config.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+    let state = {
+        loading: true
+    };
+    let tweetContent = '';
     const quoteElement = document.getElementById("quote");
     const personElement = document.getElementById("person");
     const xButton = document.getElementById("x-button");
     const newButton = document.getElementById("new-button");
+    const spinner = document.getElementById("spinner-wrapper");
+
+    const setLoading = (loadingState) => {
+        state.loading = loadingState;
+        if(loadingState) {
+            spinner.style.display = 'flex';
+        } else {
+            spinner.style.display = 'none';
+        }        
+    }
+
+    newButton.addEventListener('click', () => {
+        quoteElement.innerHTML = '';
+        personElement.innerHTML = '';
+        newButton.classList.add('disabled');
+        newButton.innerHTML = "Loading Quote";
+        getQuote();
+    })
+
+    xButton.addEventListener('click', () => { 
+        if(tweetContent.length > 0) {
+            const encodedContent = encodeURIComponent(`${tweetContent}`);
+            const twitterUrl = `https://x.com/intent/tweet?text=${encodedContent}`;
+            window.open(twitterUrl, "_blank");
+        }
+    })    
 
     const getQuote = async () => {
-        try {
-            const url = 'https://api.api-ninjas.com/v1/quotes?category=happiness'
-            const response = await fetch(url, {
-                headers: {
-                    "X-Api-Key" : KEY
-                }
-            });
+        setLoading(true);
+        const url = 'https://api.api-ninjas.com/v1/quotes?category=knowledge';
+
+        await fetch(url, {
+            method: 'GET',
+            headers: {
+                "X-Api-Key" : KEY
+            }
+        }).then(async (response) => {
             const data = await response.json();
+            if(data) {
+                setLoading(false);
+            }
             quoteElement.innerHTML = `"${data[0].quote}"`;
             personElement.innerHTML = `— ${data[0].author}`;
-        } catch(error) {
-            console.log(error);
-        }
+            tweetContent = `"${data[0].quote}" — ${data[0].author}`;
+        }).catch(() => {
+            setLoading(false);
+            quoteElement.innerHTML = "An error has occured please try again!";
+        });
+        newButton.classList.remove('disabled');
+        newButton.innerHTML = "New Quote";
     }
 
     getQuote();
